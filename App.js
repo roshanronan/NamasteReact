d3028152aaf29d3405adfb0e9f1ddd822d41844c
roshan <singhroshandu@gmail.com>
2023-01-02 09:14:31 +0530

    chapter 2 commit

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-const heading1 = React.createElement('h1',{},'Hello from React...')
-    const heading2= React.createElement('h2',{},'Hello from React...')
+import React from "react"
+import ReactDOM from 'react-dom/client'
 
-    const container = React.createElement('div',{
-        className:"container"
-    },[heading1,heading2])
-
-    const root = ReactDOM.createRoot(document.getElementById('root'))
-
-    root.render(container)
+const heading1 = React.createElement('h1',{key:1},'Hello from React Parcel...')
+const heading2= React.createElement('h2',{key:2},'Hello from React...')
+const container = React.createElement('div',{
+    className:"container"
+},[heading1,heading2])
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(container)
